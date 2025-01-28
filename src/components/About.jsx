d9194ea58cbcac6ea7e8fa255955ca5feb2ae058
Adrian Ricken <adrianricken@gmail.com>
2025-01-28 12:06:55 +0100

    edit About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,29 @@
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
-      <div className="flex flex-col justify-center items-center w-fll h-full"></div>
+      <div className="flex flex-col justify-center items-center w-fll h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              About
+            </p>
+          </div>
+          <div></div>
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>Hi. Im adrian. Nice to meet you. Please take a look around.</p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea optio
+              odio quaerat labore necessitatibus vel saepe quia iure veritatis,
+              expedita, at quibusdam. Optio, iure similique nobis ducimus a
+              excepturi rerum.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
