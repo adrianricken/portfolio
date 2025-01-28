832b5345d2dbcc73ab17f314d898a3453d39f91b
Adrian Ricken <adrianricken@gmail.com>
2025-01-28 11:04:18 +0100

    delet node modules to reinstall

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./index.css";
 import Background from "./components/Background";
 import Header from "./components/Header";
 import About from "./components/About";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <>
       <Background />
-      <Header />
+      <NavBar />
+      {/* <Header /> */}
       <About />
     </>
   );
