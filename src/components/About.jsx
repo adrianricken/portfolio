9ab32b2d796c5e753abc6e817d91736417bb7840
Adrian Ricken <adrianricken@gmail.com>
2025-01-28 10:06:00 +0100

    edit About divs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,9 @@
 const About = () => {
   return (
-    <section className="flex justify-center">
-      <div className="w-7/8 h-full bg-amber-50">
-        <div className="absolute w-1/2 h-1/2 mt-100 border bg-amber-200">
-          TEST
-        </div>
-        <div className="absolute w-1/2 h-1/2 mt-200 border bg-amber-200">
-          TEST
-        </div>
+    <section className="flex justify-center items-center w-full">
+      <div className="absolute mt-300 w-7/8 h-1/2 border">TEST</div>
+      <div className="absolute w-1/2 h-1/2 mt-800 border bg-amber-200">
+        TEST
       </div>
     </section>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,37 @@
 const Header = () => {
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:backdrop-blur-sm`}
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 backdrop-blur-sm`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src="../../public/logo.png" width={100} height={40} alt="A R" />
+          <img
+            src="../../public/logo.png"
+            width={35}
+            height={40}
+            alt="A R"
+            className="m-3"
+          />
         </a>{" "}
         <nav className="flex w-full">
-          <div className="relative z-2 flex flex-col items-center justify-around m-auto lg:flex-row">
+          <div className="w-full relative z-2 flex flex-col items-center justify-around m-auto flex-row">
             <a
               href="#"
-              className="block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-red"
+              className="block relative font-code text-2xl uppercase text-n-1 transition-opacity duration-300 hover:opacity-20"
             >
               about
             </a>
             <a
               href="#"
-              className="block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-red"
+              className="block relative font-code text-2xl uppercase text-n-1 transition-opacity duration-300 hover:opacity-20"
             >
-              projects
+              Projekte
             </a>
             <a
               href="#"
-              className="block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-red"
+              className="block relative font-code text-2xl uppercase text-n-1 transition-opacity duration-300 hover:opacity-20"
             >
-              contact
+              Kontakt
             </a>
           </div>
         </nav>
